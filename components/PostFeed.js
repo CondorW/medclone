4629b322d056dcfb34d1e96ed546b75164e61d09
CondorW <rotsteiner@gmail.com>
2022-04-04 21:38:04 +0200

    Post querying working

--- components/PostFeed.js
+++ components/PostFeed.js
@@ -1,23 +1,22 @@
 
 
 export default function PostFeed(props) {
-  let { posts, admin } = props;
-  posts = [{content:"Eins zwei drei vier",slug:"ich bin ein echtes Tier", admin:true}];
+    const {posts} = props;
 
-  return posts ? posts.map((post) => <PostItem post={post} key={post.slug} admin={admin} />) : null;
+  return posts ? posts.map((post) => <PostItem post={post} key={post.slug} />) : null;
 }
 
 function PostItem(props){
-const {post,admin,key} = props;
+const {post,key} = props;
 const wordCount = post?.content.split(" ").length;
 const readTime = (wordCount/200).toFixed(2);
 
     return(
         <div>
-            <p>hhkhkhkuji</p>
             <p>{post.content}</p>
+            <p>{post.slug}</p>
             <p>{key}</p>
-            <p>{admin}</p>
+            <p>Estimated Read Time {readTime}</p>
         </div>
     );
 }
--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,17 +1,42 @@
 import { useContext } from "react";
 import { authContext } from "../../lib/Context";
+import { getUserWithUsername, db, postToJSON } from "../../lib/firebase";
 import PostFeed from "../../components/PostFeed";
 import UserProfile from "../../components/UserProfile";
+import { collection, getDocs, limit } from "firebase/firestore";
+import { query as fbQuery } from "firebase/firestore";
 
-export default function UserPage() {
-  const {user, username} = useContext(authContext);
+
+export async function getServerSideProps({ query }) {
+  const { username } = query;
   
+
+  const userID = await getUserWithUsername(username);
+  const postRef = collection(db, `users/3XcL6borLNMZlpqufms1YUCQwA72/posts`);
+  const postQuery =  fbQuery(postRef);
+  const querySnapshot = await getDocs(postQuery);
+  const postArr = [];
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
+    postArr.push(postToJSON(doc));
+  });
+  console.log(postArr);
+
+  return {
+    props: { postArr },
+  };
+}
+
+export default function UserPage(props) {
+  const { user, username } = useContext(authContext);
+
   return (
-  <>
-  <div className="flex flex-col w-2/3 mx-auto h-screen">
-  <UserProfile user={user} username={username}></UserProfile>
-  <PostFeed></PostFeed>
-  </div>
-  </>    
+    <>
+      <div className="flex flex-col w-2/3 mx-auto h-screen">
+        <UserProfile user={user} username={username}></UserProfile>
+        <PostFeed posts={props.postArr}></PostFeed>
+      </div>
+    </>
   );
 }
