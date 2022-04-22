2fdafd6c16e248ed0dc7ee5a9402fe1eee77ed4d
CondorW <rotsteiner@gmail.com>
2022-04-22 16:06:30 +0200

    Remove unnecessary TODO

--- pages/admin/[param].js
+++ pages/admin/[param].js
@@ -1,8 +1,8 @@
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { doc, collection, query, where, getDocs } from "firebase/firestore";
 
 import Authcheck from "../../components/Authcheck";
 import { authContext } from "../../lib/Context";
-import { useContext, useState,useEffect } from "react";
+import { useContext, useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { db } from "../../lib/firebase";
 
@@ -16,9 +16,7 @@ export default function AdminParam(props) {
   const router = useRouter();
   const slug = router.query.param;
 
-  const { userID } = useContext(authContext);  
-
-  //TODO Wrap in useEffect, so that the contentState is updated when the post is fetched from the database
+  const { userID } = useContext(authContext);
 
   const getPost = async () => {
     const docRef = collection(db, `users/${userID}/posts`);
@@ -33,13 +31,21 @@ export default function AdminParam(props) {
   useEffect(() => {
     getPost();
   }, []);
-  //
+  
+  const updatePost = async () => {
+    const docRef = doc(db, `users/${userID}/posts/${slug}`);
 
+    //TODO Update the content property of the post with the contentState
+
+  };
 
   return (
     <Authcheck>
       <h1>Slug And Admin Route combined lead you to this route, Catch All</h1>
-      <textarea value={contentState} onChange={(e) => setContentState(e.target.value)}></textarea>
+      <textarea
+        value={contentState}
+        onChange={(e) => setContentState(e.target.value)}
+      ></textarea>
       <button>Save my Edits</button>
     </Authcheck>
   );
