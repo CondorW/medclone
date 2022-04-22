22c4c2de91774a3e04c2b9ec546ee09892e86b04
CondorW <rotsteiner@gmail.com>
2022-04-22 15:51:41 +0200

    Fetching a single post, according to the url-post-slug now working

--- pages/admin/[param].js
+++ pages/admin/[param].js
@@ -1,5 +1,37 @@
-export default function AdminParam(){
-    return(
-<h1>Slug And Admin Route combined lead you to this route, Catch All</h1>
-    );
+import { collection, query, where, getDocs } from "firebase/firestore";
+
+import Authcheck from "../../components/Authcheck";
+import { authContext } from "../../lib/Context";
+import { useContext } from "react";
+import { useRouter } from "next/router";
+import { db } from "../../lib/firebase";
+
+export default function AdminParam(props) {
+  //TODO Fetch the post with the url param containing the slug of the post from database
+  //Display its content in an html form, where the text area is prepopulated with the content of the post but also editable
+  //Create an update function which saves the updates made to the content property to the database
+
+  const router = useRouter();
+  const slug = router.query.param;
+
+  const { userID } = useContext(authContext);
+  console.log(userID);
+
+  const getPost = async () => {
+    const docRef = collection(db, `users/${userID}/posts`);
+    const q = query(docRef, where("slug", "==", slug));
+    const querySnapshot = await getDocs(q);
+    console.log("Database Read Executed");
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+  };
+
+  return (
+    <Authcheck>
+      <h1>Slug And Admin Route combined lead you to this route, Catch All</h1>
+      <button onClick={getPost}>Let me edit my post</button>
+    </Authcheck>
+  );
 }
